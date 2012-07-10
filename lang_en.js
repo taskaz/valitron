6df3f419398f6bbf2825ffa2c2980980b1aa62ac
ENQ <taskaz@gmail.com>
2012-07-10 08:36:09 +0300

    error language file

---
+++ lang_en.js
@@ -0,0 +1,15 @@
+$.valitron("translation", {
+	'en': {
+        'max': ":attribute field should not be greater then :max",
+        "min": {
+          "string": ":attribute should be longer then :min",
+          "number": ":attribute is to small, min is :min"
+        },
+        'attributes': {
+          "email": "E-mail",
+          "username": "Username",
+          "password": "Password",
+          "req_text" : "Text"
+        }
+      }
+  });
